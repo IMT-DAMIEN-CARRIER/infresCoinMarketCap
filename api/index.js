0b52ec2b982ffab94e72563b53bc3aaa922a5cef
Damien Carrier <80831148+MirakuSan@users.noreply.github.com>
2021-09-25 18:17:11 +0200

    Feature/recupererinfosunecrypto (#28)
    * Fonction côté api pour récupérer infos d'une crypto donnée (erreur sur symbol a fix)
    
    * Erreur liée à var symbol FIX
    
    * Correction des chaines de caractère et rectification de l'indentation
    
    * Augmentation de la tempo avant de vérifier le bon fonctionnement des applicatifs
    
    * Revert fix - a faire dans une autre branche
    
    * fix des ports sur la CI
    
    * Fix Dockerfile
    
    * correction de l'égalité
    
    * Changement du port de lancement du client
    
    * Correction de syntaxe
    
    * Rajout du endpoint '/'
    
    * Correction du nommage des images dockers et des répertoires utilisés
    
    * Suppression de dépendances
    
    * Ajout flag debug CI
    
    * augmentation de la tempo avant check
    
    * Augmentation de la tempo pour l'api et suppression des flags de test
    
    Co-authored-by: Nele <leon.bousquetpasturel@gmail.com>
    Co-authored-by: Clément <clement.savinaud@mines-ales.org>
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios').default;
+const key = '0f3f5c279b7bfe4e'
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Welcome on board !')
 });
 
+app.get(`/getinfoscoin`, async (req, res, next) => {
+  try {
+    let reqSymbol = req.query.symbol;
+    let symbol = (reqSymbol !== '') ? reqSymbol : 'BTC';
+    let res = await axios.get('https://coinlib.io/api/v1/coin', { params: { key: key, symbol: symbol } });
+    let data = res.data;
+
+    return {
+      symbol: data.symbol,
+      nom: data.name,
+      rang: data.rank,
+      prix: data.price,
+      volume_marche: data.market_cap,
+      volume_marche_24h: data.total_volume_24h,
+      prix_bas_24h: data.low_24h,
+      prix_haut_24h: data.high_24h,
+      delta_1h: data.delta_1h,
+      delta_24h: data.delta_24h,
+      delta_7d: data.delta_7d,
+      delta_30d: data.delta_30d
+    }
+  }
+  catch (err) {
+    next(err)
+  }
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 });
