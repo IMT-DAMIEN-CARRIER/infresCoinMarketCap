c03852ad913df4a26a8e6280db691634587b2ae4
Damien Carrier <damien.carrier@mines-ales.org>
2021-09-26 13:39:58 +0200

    Hotfix des requêtes /coin et /global pour ajouter le res.send oublié.

--- api/index.js
+++ api/index.js
@@ -5,53 +5,57 @@ const axios = require('axios').default;
 const key = '0f3f5c279b7bfe4e'
 
 app.get('/', (req, res) => {
-  res.send('Welcome on board !')
+    res.send('Welcome on board !')
 });
 
-app.get('/coin', (req, res, next) => {
-  let reqSymbol = req.query.symbol;
-  let symbol = (reqSymbol !== '') ? reqSymbol : 'BTC';
+app.get('/coin', (req, res) => {
+    const reqSymbol = req.query.symbol;
+    const symbol = (reqSymbol !== '') ? reqSymbol : 'BTC';
 
-  axios.get('https://coinlib.io/api/v1/coin', { params: { key: key, symbol: symbol } })
-      .then((data) => {
-        return {
-          symbol: data.symbol,
-          nom: data.name,
-          rang: data.rank,
-          prix: data.price,
-          volume_marche: data.market_cap,
-          volume_marche_24h: data.total_volume_24h,
-          prix_bas_24h: data.low_24h,
-          prix_haut_24h: data.high_24h,
-          delta_1h: data.delta_1h,
-          delta_24h: data.delta_24h,
-          delta_7d: data.delta_7d,
-          delta_30d: data.delta_30d
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    axios.get('https://coinlib.io/api/v1/coin', {params: {key: key, symbol: symbol}})
+        .then((results) => {
+            const data = results.data;
+
+            res.send({
+                symbol: data.symbol,
+                nom: data.name,
+                rang: data.rank,
+                prix: data.price,
+                volume_marche: data.market_cap,
+                volume_marche_24h: data.total_volume_24h,
+                prix_bas_24h: data.low_24h,
+                prix_haut_24h: data.high_24h,
+                delta_1h: data.delta_1h,
+                delta_24h: data.delta_24h,
+                delta_7d: data.delta_7d,
+                delta_30d: data.delta_30d
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 });
 
-app.get('/global', async (req, res, next) => {
-  let reqCurrency = req.query.currency;
-  let currency = (reqCurrency !== '') ? reqCurrency : 'USD'
+app.get('/global', (req, res) => {
+    const reqCurrency = req.query.currency;
+    const currency = (reqCurrency !== '') ? reqCurrency : 'USD'
 
-  axios.get('https://coinlib.io/api/v1/global', { params: { key: key, pref: currency} })
-      .then((data) => {
-        return {
-          coins: data.coins,
-          markets: data.markets,
-          total_market_cap: data.total_market_cap,
-          total_volume_24h: data.total_volume_24h
-        };
-      })
-      .catch((error) => {
-        console.error(error);
-      })
+    axios.get('https://coinlib.io/api/v1/global', {params: {key: key, pref: currency}})
+        .then((results) => {
+            const data = results.data;
+
+            res.send({
+                coins: data.coins,
+                markets: data.markets,
+                total_market_cap: data.total_market_cap,
+                total_volume_24h: data.total_volume_24h
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 });
