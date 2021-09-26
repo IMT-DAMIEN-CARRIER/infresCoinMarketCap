acf962dd70d3dba37e90a130d1eaf617271c5236
Clément <clement.savinaud@mines-ales.org>
2021-09-26 16:13:07 +0200

    Ajout d'un test end-to-end pour le listing des crypto

---
+++ api/test/test_coinlist.js
@@ -0,0 +1,24 @@
+var axios = require('axios');
+var assert = require('assert');
+
+describe('Test endpoint \'/\'', async function() {
+    describe('Verification generale', async function() {
+        it('Doit contenir 100 elements', async function() {
+            let data = await axios.get('http://localhost:3000/')
+                .then((results) => {
+                    return results.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                }).then((data) => {
+                    return data;
+                });
+            
+            assert.equal(data.length, 100);
+            assert.equal(Object.keys(data[0]).length, 5);
+            assert.equal(data[0].rank, 1);
+            assert.equal(data[0].symbol, 'BTC');
+            assert.equal(data[0].name, 'Bitcoin');
+        })
+    })
+})
