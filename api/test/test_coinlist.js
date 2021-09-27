da5937d974bb4f35a7b4f02cdc528433e50889b2
Clément <clement.savinaud@mines-ales.org>
2021-09-27 09:40:55 +0200

    Split des tests pour le listing des crypto

--- api/test/test_coinlist.js
+++ api/test/test_coinlist.js
@@ -15,7 +15,31 @@ describe('Test endpoint \'/\'', async function() {
                 });
             
             assert.equal(data.length, 100);
+        })
+        it('Chaque element reçu doit avoir 5 champs', async function() {
+            let data = await axios.get('http://localhost:3000/')
+                .then((results) => {
+                    return results.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                }).then((data) => {
+                    return data;
+                });
+
             assert.equal(Object.keys(data[0]).length, 5);
+        })
+        it('Le premier élément reçu contient les infos du Bitcoin', async function() {
+            let data = await axios.get('http://localhost:3000/')
+                .then((results) => {
+                    return results.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                }).then((data) => {
+                    return data;
+                });
+
             assert.equal(data[0].rank, 1);
             assert.equal(data[0].symbol, 'BTC');
             assert.equal(data[0].name, 'Bitcoin');
