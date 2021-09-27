e749ea6f1ddec0d2476458942789ab064d2cf7bf
Damien Carrier <80831148+MirakuSan@users.noreply.github.com>
2021-09-27 08:41:46 +0200

    Update client/src/App.js
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import AppAppBar from './view/AppAppBar';
-import AppFooter from "./view/AppFooter";
+import AppFooter from './view/AppFooter';
 import withRoot from "./withRoot";
 import Global from "./components/Global";
 
