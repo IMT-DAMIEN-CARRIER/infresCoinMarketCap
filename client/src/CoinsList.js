198d6889ac1f49f0c6f77cd43d6cd682b9491aef
arthur-du <arthurduca38@gmail.com>
2021-09-28 01:07:38 +0200

    récupération des informations

--- client/src/CoinsList.js
+++ client/src/CoinsList.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 import AppAppBar from './view/AppAppBar';
 import AppFooter from './view/AppFooter';
 import withRoot from './withRoot';
+import List from './components/List';
 
 function CoinsList() {
     return (
         <React.Fragment>
             <AppAppBar/>
+            <List/>
             <AppFooter/>
         </React.Fragment>
     );
