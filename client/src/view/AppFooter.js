0b1e3e227ad4e227833060eecf102a0c191c65d6
Damien Carrier <80831148+MirakuSan@users.noreply.github.com>
2021-09-27 08:59:22 +0200

    [FRONT] Récupération des info global du marché (#38)
    * [FRONT] Récupération des info global du marché
    
    * Update client/src/App.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/App.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppAppBar.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/App.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/App.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppAppBar.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppAppBar.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppFooter.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppFooter.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/components/AppBar.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/components/Global.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/components/Global.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/components/Global.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/components/Global.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppAppBar.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppAppBar.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * Update client/src/view/AppAppBar.js
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>
    
    * [FRONT] Récupération des info global du marché
    
    Co-authored-by: Toxiqh <53941570+Toxiqh@users.noreply.github.com>

---
+++ client/src/view/AppFooter.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Typography from '../components/Typography';
+
+function Copyright() {
+    return (
+        <React.Fragment>
+            {'© Léon bousquet - Damien Carrier - Arthur Duca - Clément Savinaud'}
+            {new Date().getFullYear()}
+        </React.Fragment>
+    );
+}
+
+export default function AppFooter() {
+    return (
+        <Typography
+            component="footer"
+            sx={{display: 'flex', bgcolor: 'secondary.light'}}
+        >
+            <Container sx={{my: 8, display: 'flex'}}>
+                <Grid container spacing={5} textAlign='center'>
+                    <Grid
+                        container
+                        direction='column'
+                        justifyContent='flex-end'
+                        spacing={2}
+                        sx={{height: 120}}
+                    >
+                        <Copyright/>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Typography>
+    );
+}
